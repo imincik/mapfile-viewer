93974f3f6d320bb58ef69bc54d938803c93ff0c5
Ivan Mincik <ivan.mincik@gmail.com>
2012-12-15 14:50:17 +0100

    Documentation update.

--- viewer.py
+++ viewer.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-Minimalistic HTML viewer for UMN MapServer mapfiles.
+MAPFILE DEVELOPER SCRIPT.
+Minimalistic HTML viewer for UMN MapServer mapfiles in simple
+standalone WSGI server.
 
 Example:
 	run $ ./viewer.py -m map/viewer.map
